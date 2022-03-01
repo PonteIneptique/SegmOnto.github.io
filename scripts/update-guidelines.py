4e2938618f65ea9ef664e01c97638bc1fef0347d
Thibault Clérice <1929830+PonteIneptique@users.noreply.github.com>
2022-03-01 09:52:29 +0100

    Create update-guidelines.py

---
+++ scripts/update-guidelines.py
@@ -0,0 +1,34 @@
+import yaml
+import glob
+import os.path
+
+with open("mkdocs.yml") as f:
+    yml = yaml.load(f)
+
+
+def replace(dic, keys, new_val):
+    for val in dic:
+        if keys and keys[0] in val:
+            k = keys.pop(0)
+            if keys:
+                replace(val[k], keys, new_val)
+            else:
+                val[k] = new_val
+
+
+replace(yml["nav"], ["Guidelines", "Zones"], [
+    {
+        os.path.basename(filename.replace(".md", "")): filename
+    }
+    for filename in sorted(glob.glob("./docs/gd/gdZ/**/*.md"))
+])
+
+replace(yml["nav"], ["Guidelines", "Lines"], [
+    {
+        os.path.basename(filename.replace(".md", "")): filename
+    }
+    for filename in sorted(glob.glob("./docs/gd/gdL/**/*.md"))
+])
+
+#for file in glob.glob()
+print(yaml.dump(yml))
